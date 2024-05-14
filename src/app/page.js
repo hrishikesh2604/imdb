5b85c38bb20ddc717aab4863f676610f99f60fb3
hrishikesh26 <hrishikeshdongre26@gmail.com>
2024-05-14 15:00:43 +0530

    got data

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Results from '@/components/Results';
+const API_KEY=process.env.API_KEY
+export default async function Home({searchParams}) {
+  const genre=searchParams.genre|| 'fetchTrending';
+  const res=await fetch(
+    `https://api.themoviedb.org/3${genre==='fetchTopRated' ? `/movie/top_rated`:`/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`
+   );
+   const data=await res.json();
+   if(!res.ok)
+    throw new Error('failed to fetch data') 
 
-export default function Home() {
+   const results=data.results;
+  
   return (
     <div>
-      Home
+   <Results results={results}/>
     </div>
   )
 }
