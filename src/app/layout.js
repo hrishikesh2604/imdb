c09632beadfe66cb0806df7bb105f2942fb9af01
hrishikesh26 <hrishikeshdongre26@gmail.com>
2024-05-14 18:04:13 +0530

    added search

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Header from '@/components/Header'
 const inter = Inter({ subsets: ["latin"] });
 import Providers from '@/app/providers' 
 import Navbar from "@/components/Navbar";
+import SearchBox from "@/components/SearchBox";
 export const metadata = {
   title: "IMDB-Clone",
   description: "Movie database app (IMDB Clone)",
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
         <Providers>
         <Header/>
         <Navbar/>
+        <SearchBox/>
         {children}
         </Providers>
         </body>
